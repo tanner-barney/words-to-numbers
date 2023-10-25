840665aaa0c24ae7680ab4b8eb3d5547de98371a
tanner-barney <tanner.barney@bain.com>
2023-10-25 12:36:20 -0600

    Update package dependency * Adds in talisman * Updates fuzzy code to check talisman jaro score * Removes clj-fuzzy

--- src/fuzzy.js
+++ src/fuzzy.js
@@ -1,4 +1,4 @@
-import clj_fuzzy from 'clj-fuzzy';
+import jaro from 'talisman/metrics/jaro';
 import itsSet from 'its-set';
 
 import { ALL_WORDS } from './constants';
@@ -7,7 +7,7 @@ export default (word, haystack) => {
   return (haystack || ALL_WORDS)
     .map(numberWord => ({
       word: numberWord,
-      score: clj_fuzzy.metrics.jaro(numberWord, word)
+      score: jaro.score(numberWord, word)
     }))
     .reduce((acc, stat) => !itsSet(acc.score) || stat.score > acc.score ? stat : acc, {})
     .word;
